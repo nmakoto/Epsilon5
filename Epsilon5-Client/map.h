0400f22085cd501c5d68e297f01ad943e34f8e91
Makoto Nokata <nokatamakoto@gmail.com>
2012-12-18 23:01:31 +0600

    stage0: TMap class

--- Epsilon5-Client/map.h
+++ Epsilon5-Client/map.h
@@ -1,12 +1,12 @@
 #pragma once
-
 #include <QObject>
 #include <QImage>
 #include <QPainter>
 #include <QPoint>
 #include <QColor>
-
-class TMap : public QObject {
+//------------------------------------------------------------------------------
+class TMap : public QObject
+{
     Q_OBJECT
 public:
     TMap(QObject* parent = 0);
@@ -14,13 +14,17 @@ public:
     size_t GetWidth();
     size_t GetHeight();
     QSize GetMinimapSize(quint8 maxMinimapSize);
-    QPoint GetObjectPosOnMinimap(int objectPosX, int objectPosY, quint8 maxMinimapSize);
+    QPoint GetObjectPosOnMinimap(int objectPosX, int objectPosY,
+            quint8 maxMinimapSize);
     QPoint GetObjectPosOnMinimap(QPoint objectPos, quint8 maxMinimapSize);
 
     // Draw background at correct position
-    void DrawBackground(const QPoint& playerPos, const QSize& frameSize, QPainter &painter);
+    void DrawBackground(const QPoint& playerPos, const QSize& frameSize,
+            QPainter& painter);
+
 public slots:
     void LoadMap(QString map);
+
 private:
     size_t Width;
     size_t Height;
@@ -29,3 +33,4 @@ private:
     bool Loaded;
     QPointF WHRatio;
 };
+//------------------------------------------------------------------------------
